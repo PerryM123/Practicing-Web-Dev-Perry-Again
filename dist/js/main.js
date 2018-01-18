7b1a1088637d6af433be67b3a64a034226517819
PerryM123 <perrymiranda1@yahoo.com>
2018-01-19 02:06:00 +0900

    Still half way to completing it

--- dist/js/main.js
+++
@@ -1,1 +0,0 @@
-console.log("Main.js"),console.log("Main.js2"),console.log("Main.js2"),console.log("Main.js2"),console.log("Main.js2"),console.log("Main.js2"),console.log("Main.js2"),console.log("Main.js2"),console.log("Main.js2"),console.log("Main.js2"),console.log("Main.js2");
